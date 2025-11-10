5403a4dbeb13dda97d4279f453b28617280cbf1b
Manoj Chaudhari <132763933+manoj0404@users.noreply.github.com>
2025-11-10 20:08:04 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearchIterative {
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+
+            if (arr[mid] == target) {
+                return mid; // found
+            } 
+            else if (arr[mid] < target) {
+                left = mid + 1; 
+            } 
+            else {
+                right = mid - 1; 
+            }
+        }
+
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {3, 6, 8, 12, 14, 18, 25};
+        int target = 14;
+
+        int index = binarySearch(numbers, target);
+        System.out.println("Index of " + target + ": " + index);
+    }
+}
